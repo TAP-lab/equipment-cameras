32d492969797da878ffcdc5b4e71f6325c3eba42
ajasnz <alex@ajas.nz>
2024-02-07 13:21:51 +1300

    Remove logging

--- uploader.php
+++ uploader.php
@@ -2,12 +2,8 @@
 $uploadDir = './';
 $keys = ['1234'];
 
-error_log("$_POST[key]");
 if (isset($_POST['key']) && in_array($_POST['key'], $keys)) {
-    error_log("Key OK");
-    error_log("$_POST[dir]");
     if (isset($_POST['dir'])) {
-        error_log("Dir OK");
         $targetDir = $uploadDir . $_POST['dir'] . '/';
         if (!file_exists($targetDir)) {
             mkdir($targetDir, 0777, true);
